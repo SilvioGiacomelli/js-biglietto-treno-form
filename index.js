d30c1f731d45abecc7b0826308868fb642eff2bf
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-02-21 16:10:19 +0100

    Treno

---
+++ index.js
@@ -0,0 +1,12 @@
+const inputNome = document.querySelector(".inputnome");
+const inputKm = document.querySelector(".inputkm");
+const inputAge = document.querySelector("select-age");
+const testoContenutoInput = ();
+//Bottone Reset//
+reset.addEventListener('click',function(){
+  // resetto il titolo
+  inputnome.innerHTML = testoContenutoInput;
+  // resetto il value dell'input
+  input1.value = '';
+})
+console.log()
